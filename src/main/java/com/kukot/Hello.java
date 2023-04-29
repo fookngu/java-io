f95e484402377d84389aa8549be0e7fc86282fd1
Kevin Nguyen <kevin.nguyen@contour.network>
2023-04-29 16:51:38 +0800

    whatever left over

---
+++ src/main/java/com/kukot/Hello.java
@@ -0,0 +1,13 @@
+package com.kukot;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+public enum Hello implements Closeable {
+    ;
+
+    @Override
+    public void close() throws IOException {
+
+    }
+}
--- src/test/java/BasicInputStreamTest.java
+++ src/test/java/BasicInputStreamTest.java
@@ -1,9 +1,9 @@
 import org.junit.jupiter.api.Test;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class BasicInputStreamTest {
@@ -18,5 +18,29 @@ public class BasicInputStreamTest {
             System.out.println("Total byte read = " + totalByteRead);
             assertTrue(totalByteRead > 0);
         }
+    }
+
+    @Test
+    public void writeToOutputStream() throws IOException {
+        var originalString = "This is my first output to stream";
+        try (OutputStream basicOutputStream = new FileOutputStream("dummy_out.txt")) {
+            basicOutputStream.write(originalString.getBytes(StandardCharsets.UTF_8));
+        }
+
+        try (InputStream input = new FileInputStream("dummy_out.txt")) {
+            var result = new String(input.readAllBytes());
+            assertEquals(originalString, result);
+        }
+    }
+    
+    @Test
+    public void combiningStream() throws IOException {
+        var expectedOutput = """
+                Hello, from someone reading this file.
+                This is another line
+                """;
+        var inputStream = new BufferedInputStream(new FileInputStream("dummy.txt"));
+        var fileContent = new String(inputStream.readAllBytes());
+        assertEquals(expectedOutput, fileContent);
     }
 }
